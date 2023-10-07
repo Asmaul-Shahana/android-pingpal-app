cc865e71dc9f261a47a8c40c3d8b3acd7550dbb4
Asmaul-Shahana <jucse28.360@gmail.com>
2023-10-08 02:19:26 +0600

    logo-mipmap-formats-added

--- app/src/main/java/com/example/chatapp/splash.java
+++ app/src/main/java/com/example/chatapp/splash.java
@@ -7,14 +7,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
 
+    ImageView logo, banner;
+    TextView name;
+    Animation topAnim, bottomAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logoSmall);
+        banner = findViewById(R.id.banner);
+        name = findViewById(R.id.tvAppName);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        banner.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
